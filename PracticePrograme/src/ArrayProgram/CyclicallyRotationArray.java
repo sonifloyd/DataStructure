b1445e64882d06f6a1256e99f790dd073501a814
sonifloyd <soni.floyd@gmail.com>
2020-11-24 01:21:24 -0800

    Cyclic Rotation

--- PracticePrograme/src/ArrayProgram/CyclicallyRotationArray.java
+++ PracticePrograme/src/ArrayProgram/CyclicallyRotationArray.java
@@ -1,11 +1,11 @@
-Input:  arr[] = {1, 2, 3, 4, 5}
-Output: arr[] = {5, 1, 2, 3, 4}
+/*Input:  arr[] = {1, 2, 3, 4, 5}
+Output: arr[] = {5, 1, 2, 3, 4}*/
 
-Following are steps.
+/*Following are steps.
 1) Store last element in a variable say x.
 2) Shift all elements one position ahead.
 3) Replace first element of array with x.
-
+*/
 package ArrayProgram;
 
 public class CyclicallyRotationArray {
